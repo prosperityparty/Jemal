ccdd7a2f0c595229ae70908a0f76bff1444aaf21
Xisbiga Barwaaqo ee Laantiisa Degmada Qarsa Duula <roblejanno@gmail.com>
2026-09-05 04:36:26 +0300

    Configure Supabase and whatsapp
    Configure Supabase and whatsapp

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.JEMAL_CONFIG = {
   supabaseUrl: "https://enewahdrrcdepvpfwens.supabase.co",
   supabaseKey: "HALKAN_KU_DHEJI_PUBLISHABLE_KEY",
-  whatsappNumber: "251912957760"
+  whatsappNumber: "+251912957760"
 };
